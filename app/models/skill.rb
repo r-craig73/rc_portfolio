83131336ba57745db4b3ad58e41ebed313109e55
Ron Craig <ron.craig@comcast.net>
2018-04-27 15:04:48 -0700

    fail second skill unit rspec test (1 of 2)

--- app/models/skill.rb
+++ app/models/skill.rb
@@ -1,3 +1,4 @@
 class Skill < ActiveRecord::Base
   validates :title, :presence => true
+  # validates :example, :presence => true
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 describe Skill do
   it { should validate_presence_of :title }
-  
+  it { should validate_presence_of :example }
 
 end
