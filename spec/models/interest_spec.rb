5f7393c0cd224374b4b10ea31c0335cb040c0683
Ron Craig <ron.craig@comcast.net>
2018-04-27 16:34:47 -0700

    edit interest and skill unit testing specs

--- spec/models/interest_spec.rb
+++ spec/models/interest_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 describe Interest do
   it { should validate_presence_of :heading }
   it { should validate_presence_of :body }
-
 end
--- spec/models/skill_spec.rb
+++ spec/models/skill_spec.rb
@@ -3,5 +3,4 @@ require 'rails_helper'
 describe Skill do
   it { should validate_presence_of :title }
   it { should validate_presence_of :example }
-
 end
