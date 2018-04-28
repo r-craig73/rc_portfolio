87b40a4a7038002f2a1053796b3c09a8b775b456
Ron Craig <ron.craig@comcast.net>
2018-04-27 21:31:41 -0700

    troubleshooting interests edit bug, urgh

---
+++ app/controllers/interests_controller.rb
@@ -0,0 +1,52 @@
+class InterestsController < ApplicationController
+  def index
+    @interests = Interest.all
+    render :index
+  end
+
+  def new
+    @interest = Interest.new
+  end
+
+  def create
+    @interest = Interest.new(interest_params)
+    if @interest.save
+      flash[:notice] = "Your interest is now added"
+      redirect_to interests_path
+    else
+      render :new
+    end
+  end
+
+  def show
+    @interest = Interest.find(params[:id])
+    render :display
+  end
+
+  def edit
+   @interest = Interest.find(params[:id])
+  end
+
+  def update
+    @interest = Interest.find(params[:id])
+      if @interest.update(interest_params)
+        flash[:notice] = "The interest is updated"
+        redirect_to interests_path
+      else
+        render :edit
+      end
+  end
+
+  def destroy
+    @interest = Interest.find(params[:id])
+    @interest.destroy
+    flash[:notice] = "The interest and details are removed"
+    redirect_to interests_path
+  end
+
+private
+  def interest_params
+    params.require(:interest).permit(:heading, :body)
+  end
+
+end
